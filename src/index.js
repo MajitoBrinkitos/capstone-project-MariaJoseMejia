554c252580939b7b7d30fd63d0c93d99ed796b41
Majito Brinkitos <mcvef2013@gmail.com>
2024-12-22 22:15:44 -0800

    2024-12-20

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ const app = express ();
 
 //EJS as the view engine
 app.set('view engine', 'ejs');
+//static file
+app.use(express.static("public"));
 
+//login
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+//signup
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
 
 //Listening port
 const port = 5000;
